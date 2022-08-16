4f6f1def8ff4ec2454880a259d532804ba7b3927
Bohdan Roshko <roshko.boh@gmail.com>
2022-08-16 23:40:44 +0300

    HotFix

--- bArt.InterviewTask/DataModels/Account.cs
+++ bArt.InterviewTask/DataModels/Account.cs
@@ -1,8 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-
-namespace bArt.InterviewTask.DataModels
+﻿namespace bArt.InterviewTask.DataModels
 {
     public class Account
     {
--- bArt.InterviewTask/DataModels/Incident.cs
+++ bArt.InterviewTask/DataModels/Incident.cs
@@ -1,12 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
-namespace bArt.InterviewTask.DataModels
+﻿namespace bArt.InterviewTask.DataModels
 {
     public class Incident
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
         public string IncidentName { get; set; }
         public string? IncidentDescription { get; set; }
         public ICollection<Account> Accounts { get; set; }
